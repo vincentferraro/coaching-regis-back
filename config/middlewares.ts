9a7b3dec99029436fee4ebb7efcc5127f2ea3625
vincentferraro <v.ferraro@live.fr>
2025-09-30 12:02:55 +0200

    Update: adding cors and webhook

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,11 +2,20 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [process.env.FRONT_END_URL, 'http://localhost:3000' ],
+      methods: ['GET', 'POST', 'PUT','OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
 ];
